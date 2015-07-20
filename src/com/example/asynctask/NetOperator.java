fd095455c2f0e91be5eb526ac465f24068dde6c4
linkun.lk <linkun.lk@alibaba-inc.com>
2015-07-20 16:42:03 +0800

    AsyncTask example

---
+++ src/com/example/asynctask/NetOperator.java
@@ -0,0 +1,16 @@
+package com.example.asynctask;
+
+//Ä£ÄâÍøÂç»·¾³  
+public class NetOperator {  
+    
+  public void operator(){  
+      try {  
+          //ĞİÃß1Ãë  
+          Thread.sleep(1000);  
+      } catch (InterruptedException e) {  
+          // TODO Auto-generated catch block  
+          e.printStackTrace();  
+      }  
+  }  
+
+}  
